802138d38c0d17078161687185b8472dc7fe4143
bettyreaney <breaney@mail.sfsu.edu>
2018-05-01 18:13:00 -0700

    Stars now fill in when clicked

--- FinalApplication/ViewController.swift
+++ FinalApplication/ViewController.swift
@@ -19,6 +19,9 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
 
     @IBOutlet weak var photoImageView: UIImageView!
     
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    
     
     override func viewDidLoad() {
         
@@ -91,38 +94,9 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         
     }
     
-    
-    
-    
-    
-  //  @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
-        
-        // Hide the keyboard.
-        
-      //  nameTextField.resignFirstResponder()
-        
-        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
-        
-      //  let imagePickerController = UIImagePickerController()
-        
-        // Only allow photos to be picked, not taken.
-        
-      //  imagePickerController.sourceType = .photoLibrary
-        
-        // Make sure ViewController is notified when the user picks an image.
-        
-       // imagePickerController.delegate = self
-      //  present(imagePickerController, animated: true, completion: nil)
-    
-    
-    }
+}
 
 
-  //  @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        
-       // restaurantNameLabel.text = "Default Text"
-   // }
-    
 
-//}
+
 
